1e90f80b0179c78b9052912b3be6720131e2fff8
Jehan <jehanguiang09@gmail.com>
2026-01-11 21:46:52 +0100

    initial commit

---
+++ src/main/java/com/jehan/aid_kit/AidKitApplication.java
@@ -0,0 +1,13 @@
+package com.jehan.aid_kit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidKitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AidKitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jehan/aid_kit/AidKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jehan.aid_kit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidKitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
